032feaeb2a760df193d850b500d2af3645585a6f
Manivannan  Senthil <manivannan.senthil@gmail.com>
2025-05-16 16:14:17 -0700

    updates next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"], // or whatever port you're using
+  },
+};
 
 export default nextConfig;
